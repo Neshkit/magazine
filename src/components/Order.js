a99eb0996ba01a18b460a39336a84096700229c1
Nikita <nikitagracev111@gmail.com>
2026-04-12 18:28:32 +0300

    Fix CI build: add missing img alt in Order

--- src/components/Order.js
+++ src/components/Order.js
@@ -3,12 +3,13 @@ import { FaTrash} from 'react-icons/fa'
 
 export class Order extends Component {
   render() {
+    const { item, onDelete } = this.props
     return (
       <div  className ='item'> 
-        <img src={"./img/" + this.props.item.img} />
-        <h2>{this.props.item.title}</h2>
-        <p>{this.props.item.price}₽</p>
-        <FaTrash className='delete-icon' onClick={()=> this.props.onDelete(this.props.item.id)} />
+        <img src={"./img/" + item.img} alt={item.title || ''} />
+        <h2>{item.title}</h2>
+        <p>{item.price}₽</p>
+        <FaTrash className='delete-icon' onClick={() => onDelete(item.id)} />
       </div>
     )
   }
